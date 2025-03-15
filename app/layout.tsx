bb79c440d20c1446974499a4b41848cedf088dbf
davi-oliver <davi.oliveira.teixera@unifei.edu.br>
2025-03-15 11:45:41 -0300

    feat login + langing page

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css"
+import { Inter } from "next/font/google"
+ 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import type React from "react"
+import { ThemeProvider } from "./components/theme"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Minimal Creative Agency",
+  description: "Apple-inspired design portfolio",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} min-h-screen bg-background text-foreground`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Header />
+            <main className="flex flex-col items-center justify-center my-16">{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
